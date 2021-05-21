873fcada301650f575628902705194d6d8093a8f
Moro-max <81566526+Moro-max@users.noreply.github.com>
2021-05-21 20:39:23 +0000

    Create projet1.py

---
+++ projet1.py
@@ -0,0 +1,22 @@
+
+
+# EXERCICE 1 (CREE DES OBJETS NATIFS)
+
+chaine=str()
+entier=int()
+decimal=float()
+booleen=bool()
+
+# EXERCICE 2 (CORRIGER LES ERREUR DE CHAINE DE CARACTERE)
+
+print("Bonjour tout le monde!")
+print("Je m'appelle Patrick.")
+print("Je dis souvent la vie un long fleuve tranquille quand on s'appelle Patrick.")
+print('oui,je suis très philosophe.')
+
+# EXERCICE 3 (CORRIGER LES VARIABLES)
+
+a=-2.5
+b=True
+c=False
+d="Bonjour tou le monde"
