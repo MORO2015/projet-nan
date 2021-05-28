6470ddf0a8260e33c359e63b151d152f91c0d895
Moro-max <81566526+Moro-max@users.noreply.github.com>
2021-05-28 00:34:26 +0000

    Create input_user.py

---
+++ input_user.py
@@ -0,0 +1,7 @@
+
+################### EXO #########################
+
+a=str(input("Entrez votre prénom :"))
+b=str(input("Entrez votre ville de naissance :"))
+c=int(input("Entrez votre age :"))
+print(f" Votre prénom est: {a} \n Votre ville de naissance est: {b} \n Votre age est: {c} ans")
